fe8d79cc5c842dca1df14cf884abaac86ee8a0bb
Dylan Morris <dylanmorris330@gmail.com>
2022-07-03 00:19:20 +0100

    Updated version number

--- EventAnnouncements/Plugin.cs
+++ EventAnnouncements/Plugin.cs
@@ -10,7 +10,7 @@ namespace EventAnnouncements
 
         public override string Name => "EventAnnouncements";
         public override string Author => "Heisenberg3666";
-        public override Version Version => new Version(1, 0, 0, 0);
+        public override Version Version => new Version(1, 1, 0, 0);
         public override Version RequiredExiledVersion => new Version(5, 2, 2);
 
         public override void OnEnabled()
